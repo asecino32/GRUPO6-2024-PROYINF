a3569faf3fc44f27489d036a1384969f272fae51
asecino32 <efrprado@gmail.com>
2024-10-18 19:25:07 -0300

    carga de pagina

---
+++ ProyA/tasks/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    tipo_usuario = models.SmallIntegerField(blank=False)
+    nombre_usuario = models.CharField(max_length=50, unique=True, blank=False)
+    password =  models.CharField(max_length=50, blank=False)
+    correo_usuario = models.CharField(max_length=100, unique=True, blank=False)
+
+class Fuente(models.Model):
+    id_fuente = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=1000)
+    fuente_activa = models.BooleanField(blank=False)
+    url = models.CharField(max_length=2000)
+    def __str__(self):
+        return f"{self.titulo} {self.url} {self.fuente_activa}"
+
+
+class Boletin(models.Model):
+    id_boletin = models.AutoField(primary_key=True)
+    fecha_creacion = models.DateField(blank=False)
+    titulo = models.CharField(max_length=100)
+    ciudad_tratada = models.CharField(max_length=100)
+    tematica = models.CharField(max_length=100)
+    fuente_boletin = models.ForeignKey(Fuente, on_delete=models.CASCADE, related_name='fuente')
+    
