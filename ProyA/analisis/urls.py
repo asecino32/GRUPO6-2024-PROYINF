271536d7b04100bc350e0cb14b13f654a651dd14
asecino32 <efrprado@gmail.com>
2024-10-18 22:26:05 -0300

    incluir login register incompletos

--- ProyA/analisis/urls.py
+++ ProyA/analisis/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('tasks.urls'))
+    path('index/', include('tasks.urls')),
 ]
